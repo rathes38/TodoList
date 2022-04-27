f03dcac692e67c1b3fee47aa028c3a778c548faa
Ratheesh.RL <Ratheesh.RL@conduent.com>
2022-04-27 14:50:32 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ratheesh R L on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
